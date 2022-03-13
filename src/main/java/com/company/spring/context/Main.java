adbeee42456ec3744c1199b622cc318298867804
s-babaei <sara.babaei.8941@gmail.com>
2022-03-13 13:54:50 -0700

    Second commit by sara

--- src/main/java/com/company/spring/context/Main.java
+++ src/main/java/com/company/spring/context/Main.java
@@ -9,10 +9,45 @@ public class Main {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        StudentPrototype prototype = (StudentPrototype) context.getBean("prototype");
-        StudentSingleton singleton = (StudentSingleton) context.getBean("singleton");
-        System.out.println("Sara");
 
+
+//        StudentPrototype prototype = (StudentPrototype) context.getBean("prototype");
+//        StudentPrototype prototype1 = (StudentPrototype) context.getBean("prototype");
+//        StudentPrototype prototype2 = (StudentPrototype) context.getBean("prototype");
+//        StudentPrototype prototype3 = (StudentPrototype) context.getBean("prototype");
+//        StudentPrototype prototype4 = (StudentPrototype) context.getBean("prototype");
+//        StudentPrototype prototype5 = (StudentPrototype) context.getBean("prototype");
+
+
+        StudentSingleton singleton = (StudentSingleton) context.getBean("singleton");
+        StudentSingleton singleton1 = (StudentSingleton) context.getBean("singleton");
+        if (singleton1==singleton)//reference
+            System.out.println("HAHA");
+
+        singleton.setName("sara");
+        singleton1.setFamily("ghaderi");
+
+        System.out.println("////////////////////////////////////////////////////////");
+
+        System.out.println(singleton);
+        System.out.println(singleton1);
+
+        StudentSingleton singleton2 = singleton1;
+
+
+
+
+//       StudentSingleton singleton1 = new StudentSingleton(25,"ahmmad","ahmmadi");
+//
+//
+//        //object management
+//
+//        System.gc();
+//
+//
+//
+       System.out.println("Sara");
+//
 
     }
 }
---
+++ src/main/java/com/company/spring/context/StudentSara.java
@@ -0,0 +1,18 @@
+package com.company.spring.context;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentSara {
+
+    StudentPrototype studentPrototype;
+    String name;
+
+}
