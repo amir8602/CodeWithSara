81b268aca979b1978a86a926f10be71d68631acb
amir8602 <amirreza8602@gmail.com>
2022-03-13 19:47:04 +0330

    AmirSe2 second commit add unit test for singleton and prototype

--- src/test/java/com/company/AppTest.java
+++ src/test/java/com/company/AppTest.java
@@ -2,19 +2,40 @@ package com.company;
 
 import static org.junit.Assert.assertTrue;
 
+import com.company.spring.context.StudentPrototype;
+import com.company.spring.context.StudentSingleton;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
+    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+
+
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
+
+    @Test
+    public void checkSingletonBean(){
+        StudentSingleton singleton = (StudentSingleton) context.getBean("singleton");
+        StudentSingleton singleton1 = (StudentSingleton) context.getBean("singleton");
+        assertTrue(singleton==singleton1);
+    }
+
+    @Test
+    public void checkPrototypeBean(){
+        StudentPrototype prototype = (StudentPrototype) context.getBean("prototype");
+        StudentPrototype prototype1 = (StudentPrototype) context.getBean("prototype");
+        assertTrue(prototype!=prototype1);
+    }
+
 }
